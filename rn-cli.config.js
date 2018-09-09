919f8b9f1e73ef518eed2f38419c9b84335e227f
krister <krister@sent.at>
2018-09-09 16:57:38 +0300

    remove react native fixtures workaround

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,9 +1,4 @@
-const blacklist = require("metro/src/blacklist");
-
 module.exports = {
-  getBlacklistRE() {
-    return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
-  },
   getTransformModulePath() {
     return require.resolve("react-native-css-transformer");
   },
