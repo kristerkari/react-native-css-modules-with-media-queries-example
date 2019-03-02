7e0a2f1b0ddf6a82c1e5b601d5dd0f9714485da1
krister <krister@sent.at>
2019-03-02 20:05:14 +0200

    Use configFile: false for babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,62 +4,67 @@ const path = require("path");
 module.exports = {
   entry: ["./polyfills", "react-hot-loader/patch", "./index.web.js"],
   devServer: {
-    hot: true,
+    hot: true
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.ttf$/,
-        loader: "url-loader",
+        loader: "url-loader"
       },
       {
         test: /\.js?$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
+          configFile: false,
           babelrc: false,
-          presets: ["@babel/env", "react", "module:metro-react-native-babel-preset"],
-          plugins: ["react-hot-loader/babel"],
-        },
+          presets: [
+            "@babel/env",
+            "react",
+            "module:metro-react-native-babel-preset"
+          ],
+          plugins: ["react-hot-loader/babel"]
+        }
       },
       {
         test: /\.(jpg|png|svg)$/,
         use: {
           loader: "file-loader",
           options: {
-            name: "[path][name].[hash].[ext]",
-          },
-        },
+            name: "[path][name].[hash].[ext]"
+          }
+        }
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: "style-loader"
           },
           {
             loader: "css-loader",
             options: {
               modules: true,
-              localIdentName: "[path]___[name]__[local]___[hash:base64:5]",
-            },
+              localIdentName: "[path]___[name]__[local]___[hash:base64:5]"
+            }
           },
           {
-            loader: "postcss-loader",
-          },
-        ],
-      },
-    ],
+            loader: "postcss-loader"
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     alias: {
-      "react-native": "react-native-web",
+      "react-native": "react-native-web"
     },
     extensions: [".web.js", ".js", ".web.jsx", ".jsx"],
-    mainFields: ["browser", "main"],
-  },
+    mainFields: ["browser", "main"]
+  }
 };
