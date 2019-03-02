3fbf4bce896633daaaa9f77df4f691ce1be56446
krister <krister@sent.at>
2019-03-02 19:58:23 +0200

    Update configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"]
-}
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: ["react-native-classname-to-dynamic-style"],
+  env: {
+    production: {
+      plugins: ["transform-remove-console"]
+    }
+  }
+};
